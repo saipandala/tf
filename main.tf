0460c235b105ffd7085b191ee5f36539b58bc222
arun12cool <41614057+arun12cool@users.noreply.github.com>
2022-02-27 18:02:06 +0530

    test
    test

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "paas-cp" {
+bucket = "paas-cp-s3-remote"
+tags = {
+    Name        = "arun"
+    Environment = "Dev"
+    }
+}
+resource "aws_s3_bucket_acl" "acl-test" {
+    bucket = aws_s3_bucket.paas-cp.id
+    acl    = "private"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  
+}
+
+terraform {
+  backend "s3" {
+    bucket = "paas-us-west-2-development-tfstate-bucket"
+    key    = "s3-test-1-remote"
+    region = "us-west-2"
+    shared_credentials_file = "aws-creds.ini"
+    workspace_key_prefix = "crossplane-test-remote-1"
+  }
+}
